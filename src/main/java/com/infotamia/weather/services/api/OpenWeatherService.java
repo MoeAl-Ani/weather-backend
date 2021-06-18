6662d3be9a8273a1041a7ddee02d722268e768dd
Mohammed Al-Ani <mohammedalanny@gmail.com>
2021-06-18 18:59:45 +0300

    - update jwt to 10 days.

--- src/main/java/com/infotamia/weather/services/api/OpenWeatherService.java
+++ src/main/java/com/infotamia/weather/services/api/OpenWeatherService.java
@@ -1,9 +1,11 @@
 package com.infotamia.weather.services.api;
 
+import com.infotamia.weather.access.AbstractPrincipal;
 import com.infotamia.weather.exception.BaseErrorCode;
 import com.infotamia.weather.exception.IncorrectParameterException;
 import com.infotamia.weather.pojos.entities.WeatherFavouriteSearchEntity;
 import com.infotamia.weather.pojos.entities.WeatherResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -12,13 +14,17 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author Mohammed Al-Ani
  */
 @Service
 public class OpenWeatherService implements WeatherService {
-    final Set<WeatherFavouriteSearchEntity> favourites = Collections.synchronizedSet(new HashSet<>());
+    final Map<Integer, Set<WeatherFavouriteSearchEntity>> favourites = new ConcurrentHashMap<>();
+
+    @Autowired
+    AbstractPrincipal user;
 
     @Override
     public WeatherResult getWeatherByCity(
@@ -36,28 +42,33 @@ public class OpenWeatherService implements WeatherService {
     public WeatherFavouriteSearchEntity saveFavourite(
             @NotNull(message = "search term model can not be null")
             @Valid WeatherFavouriteSearchEntity model) {
-        synchronized (favourites) {
-            favourites.add(model);
-        }
+        favourites.put(user.getId(), new HashSet<>());
+        favourites.computeIfPresent(user.getId(), (k, h) -> {
+            h.add(model);
+            return h;
+        });
         return model;
     }
 
     @Override
     public List<WeatherFavouriteSearchEntity> getFavourites() {
-        return new ArrayList<>(favourites);
+        Set<WeatherFavouriteSearchEntity> weatherFavouriteSearchEntities = favourites.get(user.getId());
+        if (weatherFavouriteSearchEntities == null) {
+            return new ArrayList<>();
+        }
+        return new ArrayList<>(weatherFavouriteSearchEntities);
     }
 
     @Override
     public void deleteFavourite(
             @NotNull(message = "city is mandatory")
-            @NotEmpty(message = "city must not be empty") String city) throws IncorrectParameterException {
+            @NotEmpty(message = "city must not be empty") String city) {
 
         WeatherFavouriteSearchEntity fav = new WeatherFavouriteSearchEntity(city);
-        if (!favourites.contains(fav)) {
-            throw new IncorrectParameterException("delete failed, provided city was not found!", BaseErrorCode.INVALID_PARAMETERS);
-        }
-
-        favourites.remove(fav);
+        favourites.computeIfPresent(user.getId(), (k,v) -> {
+            v.remove(fav);
+            return v;
+        });
     }
 
 }
